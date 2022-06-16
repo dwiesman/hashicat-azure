bc63662d21cde613dfe0a5ba46eca1e523aaaa3d
Dave Wiesman <dave.wiesman@gmail.com>
2022-06-16 19:57:34 +0000

    Corrected resource_group_name

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/example-org-eb7b0a/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "hashicat-azure-dnw-network"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
